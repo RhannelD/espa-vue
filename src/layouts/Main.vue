8b0480168a912995f07ca8b11a00ee67bd428d04
RhannelD <rhanneldinlasan@gmail.com>
2023-01-16 12:14:49 +0800

    delete unnecessary default files

---
+++ src/layouts/Main.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <Topbar/>
+
+        <div class="m-4">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import Topbar from './Topbar.vue';
+</script>
